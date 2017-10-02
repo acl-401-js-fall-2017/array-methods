c01795133422aa12631f255f725e127c61301d9e
David Nishikawa <triptype@gmail.com>
2017-10-02 16:00:00 -0700

    add forEach methods and test

---
+++ test/forEach.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const forEach = require('../lib/forEach');
+
+describe('array methods', () => {
+    it('forEach', () => {
+        const array = ['a', 'b', 'c'];
+        var called = '';
+
+        forEach(array, (letter, index) => {
+            called += letter + index;
+        });
+        assert.equal(called, 'a0b1c2');
+    });
+});
