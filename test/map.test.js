6cc3210fe838ee8519c624087841a0243d7eafdd
David Nishikawa <triptype@gmail.com>
2017-10-02 16:17:07 -0700

    add map function and test

---
+++ test/map.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const map = require('../lib/map');
+
+describe('map function', () => {
+    it('returns a modified array', () => {
+        const originalArray = [1, 2, 3, 4];
+        const mappedArray = map(originalArray, (num, index) => {
+            return num * index;
+        });
+        assert.deepEqual(mappedArray, [0, 1, 6, 12]);
+    });
+});
