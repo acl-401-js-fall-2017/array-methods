0659737e65d901d5635f3f43d92ea81485bed190
Christina Mills <chrimills@gmail.com>
2017-10-03 13:05:56 -0700

    indexfinder failing test

--- test/arrayTests.js
+++ test/arrayTests.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const map = require('../lib/map');
 const filter = require('../lib/filter');
-
+const indexFinder = require('../lib/indexFinder')
 
 describe('map', () => {
     it('makes new array with result of calling fnctn every element', () => {
@@ -17,7 +17,7 @@ describe('map', () => {
 describe('filter', () => {
     const testArr = [1, 5, 6, 7];
     const filteredArr = filter(testArr, (item) => item > 1); //saving result to filteredArr
-    it.only('creates new array of elements that meet filter criteria', () => {
+    it('creates new array of elements that meet filter criteria', () => {
         // const filterCallback = function (item, index) {
         //     if (item > 1) return item;
         // }
@@ -25,4 +25,14 @@ describe('filter', () => {
         // const filteredArr = filter([1, 2, 3, 4], filterCallback);
         assert.deepEqual(filteredArr, [5, 6, 7]);
     })
+});
+
+describe('indexFinder', () => {
+    const testArr = [1, 3, 2, 4];
+    const cond = indexFinder(testArr, (item) => item > 2);
+    it.only('returns index of first element that passes condition', () => {
+
+        assert.deepEqual(cond, 1);
+    })
+
 });
