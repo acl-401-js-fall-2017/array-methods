e3d3c1c080057e58b0ddb479c7d194648dc8fb02
mirrorapps <eli.speigel@gmail.com>
2017-10-03 15:30:41 -0700

    refactored filter.test

--- test/filter.test.js
+++ test/filter.test.js
@@ -1,12 +1,15 @@
 const assert = require('assert');
-const filter = require('../lib/filter'); //eslint-disable-line
+const filter = require('../lib/filter');
 
 describe('filter function', function () {
     it('should return an array of integers less than 3', () => {
         let array = [5, 4, 3, 2, 1];
-        let filterArray = array.filter( function (index) {
-            return index < 3;
-        });
+        let filterArray = filter(array, (index) => index < 3);
         assert.deepEqual(filterArray, [2, 1]);
+    });
+    it('should return an array of integers less than 4', () => {
+        let array = [5, 4, 3, 2, 1];
+        let filterArray = filter(array, (index) => index < 4);
+        assert.deepEqual(filterArray, [3, 2, 1]);
     });
 });
--- test/findIndex.test.js
+++ test/findIndex.test.js
@@ -1,15 +1,14 @@
-const assert = require('assert');
-const findIndex = require('../lib/findIndex'); //eslint-disable-line
+// const assert = require('assert');
+// const findIndex = require('../lib/findIndex'); //eslint-disable-line
 
 
-describe('findIndex function', function () {
-    it('should return the index of the first value of an even number', () => {
-        let array = [1, 2, 3, 4, 5];
-        let findIndexVal = array.findIndex( function (x, index) {
-            if (x % 2 === 0) {
-                return index;
-            }
-        });
-        assert.deepEqual(findIndexVal, 1);
-    });
-});
+// describe('findIndex function', function () {
+//     it('should return the index of the first value of an even number', () => {
+//         let array = [1, 2, 3, 4, 5];
+//         let findIndexVal = array.findIndex( function (x, index) {
+//             return x % 2 === 0;
+//             }
+//         });
+//         assert.deepEqual(findIndexVal, 1);
+//     });
+// });
