df21083b46556bf0e6337b04aac231f73f92b127
ckrountree <jmail0207@gmail.com>
2017-10-03 13:22:12 -0700

    start writiing test for reduce array method

--- test/every.test.js
+++ test/every.test.js
@@ -1,11 +1,9 @@
 const assert = require('assert');
 const every = require('../lib/every');
 
-const callback = (ele) => ele > 10;
-
 it('every', () => {
     const initArr = [7, 49, 68];
-    const test = every(initArr, callback);
+    const test = every(initArr, (ele) => ele > 10);
 
     assert.equal(test, false);
 });
---
+++ test/reduce.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+const reduce = require('../lib/reduce');
+
+it('reduce', () => {
+    const origArr = [1, 2, 3];
+    const test = reduce(origArr, callback, initValue);
+})
