9958dd391a5a6cb1dc0e9a3e4013ee37fc60b683
Zac-L <Zachlichens@gmail.com>
2017-10-03 13:18:26 -0700

    added white space

--- test/map.test.js
+++ test/map.test.js
@@ -3,8 +3,10 @@ const map = require('../lib/map');
 
 describe('mapping arrays!', () => {
     it('mapping', () => {
+
         const array = [ 8 , 9 , 10 ];
         const mapped = map( array, x => x + x );
+        
         assert.deepEqual( mapped, [ 16 , 18 , 20 ] );
     });
 });
