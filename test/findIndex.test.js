15a477b38fc45ed62833fe4cbae7e600f1193319
Zac-L <Zachlichens@gmail.com>
2017-10-03 11:32:22 -0700

    findIndex is now passing

--- test/findIndex.test.js
+++ test/findIndex.test.js
@@ -3,8 +3,8 @@ const findIndex = require('../lib/findIndex');
 
 describe('looking for index of array', () => {
     it('returns the index that was found', () => {
-        const testArray = [ 5 , 10 , 20 ];
-        const found = findIndex(testArray, x => x > 5);
+        const testArray = [ 10 , 20 , 30 ];
+        const found = findIndex(testArray, x => x > 10);
         assert.equal(found, 1);
 
     });
