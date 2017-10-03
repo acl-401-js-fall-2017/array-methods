ac558f68701878f3e295ac4966fbd9d4011ce281
Zac-L <Zachlichens@gmail.com>
2017-10-03 11:03:53 -0700

    map test is passing

---
+++ test/map.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const map = require('../lib/map');
+
+describe('mapping arrays!', () => {
+    it('mapping', () => {
+        const array = [ 8 , 9 , 10 ];
+        const mapped = map( array, x => x + x );
+        assert.deepEqual( mapped, [ 16 , 18 , 20 ] );
+    });
+});
