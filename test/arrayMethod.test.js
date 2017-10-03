c3a9f457cf5af5cd1dafabb3fe4c23e7b5e98f2a
mugworks <greenwood.pdx@gmail.com>
2017-10-03 09:32:32 -0700

    Filter test

--- test/arrayMethod.test.js
+++ test/arrayMethod.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const forEach = require('../lib/forEach');
 const map = require('../lib/map');
+const filter = require('../lib/filter');
 
 
 describe('array methods', () => {
@@ -13,11 +14,20 @@ describe('array methods', () => {
         assert.equal(called, 'a0b1c2');
     });
 
-    it.only('map', () => {
+    it('map', () => {
         const array = [2, 3, 6];
         const mapArray = map(array, (num, index) => {
             return num * num;
         });
         assert.deepEqual(mapArray, [4, 9, 36]);
     });
+
+    it.only('filter', () => {
+        const array = [2, 5, 8];
+        const filterArray = filter(array, (num, index) => {
+            return num % 2 === 0;
+        });
+        assert.deepEqual(filterArray, [2, 8]);
+    })
+
 });
