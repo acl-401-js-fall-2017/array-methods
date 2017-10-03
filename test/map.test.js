ccdb048fc443bcae94557809bc196e8aa007a4ee
mirrorapps <eli.speigel@gmail.com>
2017-10-03 10:57:31 -0700

    map test passes

--- test/map.test.js
+++ test/map.test.js
@@ -5,8 +5,8 @@ describe('map function', () => {
     it('an array of integers raised to the power of their index', () => {
         const array = [1, 2, 3, 4, 5];
         const mapArray = map(array, (int, index) => {
-            return Math.pow(int, index);
+            return int * index;
         });
-        assert.deepEqual(mapArray, [1, 2, 9, 64, 625]);
+        assert.deepEqual(mapArray, [0, 2, 6, 12, 20]);
     });
 });
