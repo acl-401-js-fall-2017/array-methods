7e4c1319d65a34ddb4e2d48dd060cc74fbe41d85
Zac-L <Zachlichens@gmail.com>
2017-10-03 10:53:04 -0700

    Just a test to make sure mocha is working

--- test/array.test.js
+++ test/array.test.js
@@ -1,1 +1,9 @@
 const assert = require('assert');
+
+describe('Test', () => {
+    it('running console log', () => {
+        
+        // leaving this console log in for now will take out once all methods have passed.
+        console.log('mocha is working');
+    });
+});
