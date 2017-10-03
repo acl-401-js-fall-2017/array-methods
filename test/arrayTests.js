7a532fc161c73781917528364837f53d95c06f70
Christina Mills <chrimills@gmail.com>
2017-10-03 11:19:43 -0700

    map test passing

--- test/arrayTests.js
+++ test/arrayTests.js
@@ -1,9 +1,14 @@
 const assert = require('assert');
 const map = require('../lib/map');
 
+
 describe('map', () => {
     it('makes new array with result of calling fnctn every element', () => {
-        assert.equal(map([1, 2, 3, 4]), [1, 4, 6, 8] );
+        var mapCallback = function (item, index) {
+            return item * 2;
+        }
+        var mappedArr =  map([1,2,3,4], mapCallback);
+        assert.deepEqual(mappedArr, [2, 4, 6, 8]);
     })
 
 });
