eb86542f407863db7823bfb99a4206e1959fd358
Maryussuperboy <martsyalis@gmail.com>
2017-10-03 11:01:45 -0700

    wrote a test and a passing function for filter

--- test/array-method-tests.js
+++ test/array-method-tests.js
@@ -5,11 +5,25 @@ const assert = require('assert');
 describe ('map', function() {
     const testArray = ['a','b','c','d'];
     it('should create a new array', function(){
-        const newArray = arrayMethods.map(testArray,(value,i) => {
+        let newArray = arrayMethods.map(testArray,(value,i) => {
             return value += i;
         });
+
         assert.deepEqual(newArray,['a0','b1','c2','d3']);
         assert.deepEqual(newArray.length,4);
     });
 });
+
+describe ('filter', function() {
+    const testArray = ['aaaaaaa','b','ccccccc','d'];
+    it('should create a new array with all items whose callback returned truthy values', function(){
+        let newArray =  arrayMethods.filter(testArray,(value) => {
+            return value.length >5;
+        });
+
+        assert.deepEqual(newArray,['aaaaaaa','ccccccc']);
+        assert.deepEqual(newArray.length,2);
+    });
+});
+
 
