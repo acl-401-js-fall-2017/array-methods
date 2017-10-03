cf85e05fd5e4a33e000fd04684ca4a8f44524dca
Zac-L <Zachlichens@gmail.com>
2017-10-03 13:17:34 -0700

    added a little white space

--- test/findIndex.test.js
+++ test/findIndex.test.js
@@ -3,8 +3,10 @@ const findIndex = require('../lib/findIndex');
 
 describe('looking for index of array', () => {
     it('returns the index that was found', () => {
+
         const testArray = [ 10 , 20 , 30 ];
         const found = findIndex(testArray, x => x > 10);
+
         assert.equal(found, 1);
 
     });
