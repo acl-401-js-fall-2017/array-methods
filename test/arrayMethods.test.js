965f60b86c22a5d2f8efeb92818a33fabaa8a972
Robin Miller <rmillerpdx@gmail.com>
2017-10-02 18:39:51 -0700

    create working code for every method

--- test/arrayMethods.test.js
+++ test/arrayMethods.test.js
@@ -4,7 +4,7 @@ const every = require('../lib/every');
 
 describe('array methods', () => {
     it('forEach', () => {
-        const array = ['a', 'b', 'c'];
+        let array = ['a', 'b', 'c'];
         let called = '';
         forEach(array, (letter, index) => {
             called += letter + index;
@@ -13,8 +13,12 @@ describe('array methods', () => {
     });
 
     it('every', () => {
-        const array = [1, 2, 3];
-        let moreThanZero = function(a) {return a > 0;};
-        assert.equal(every(array, moreThanZero), true);
+        let array = [1, 2, 3];
+        function moreThanZero(a){
+            if (a > 0) return true;
+            else return false;
+        }
+        let results = every(array, moreThanZero);
+        assert.equal(results, true);
     });
 });
