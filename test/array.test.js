97d26bc692add83b0b9b01631a840c14db1e84d0
Zac-L <Zachlichens@gmail.com>
2017-10-03 13:22:30 -0700

    do not need this anymore

--- test/array.test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-
-describe('Test', () => {
-    it('running console log', () => {
-        
-        // leaving this console log in for now will take out once all methods have passed.
-        console.log('mocha is working');
-    });
-});
