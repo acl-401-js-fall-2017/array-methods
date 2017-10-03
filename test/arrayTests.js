7f32ba7ec1f6df55b76240a2509823c2e8ac9ed7
Christina Mills <chrimills@gmail.com>
2017-10-03 12:04:39 -0700

    got filter to work

--- test/arrayTests.js
+++ test/arrayTests.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const map = require('../lib/map');
+const filter = require('../lib/filter');
 
 
 describe('map', () => {
@@ -11,4 +12,17 @@ describe('map', () => {
         assert.deepEqual(mappedArr, [2, 4, 6, 8]);
     })
 
+});
+
+describe('filter', () => {
+    const testArr = [1, 5, 6, 7];
+    const filteredArr = filter(testArr, (item) => item > 1); //saving result to filteredArr
+    it.only('creates new array of elements that meet filter criteria', () => {
+        // const filterCallback = function (item, index) {
+        //     if (item > 1) return item;
+        // }
+
+        // const filteredArr = filter([1, 2, 3, 4], filterCallback);
+        assert.deepEqual(filteredArr, [5, 6, 7]);
+    })
 });
