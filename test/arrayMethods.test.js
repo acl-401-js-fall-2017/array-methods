7ffa7541ad4d6bb28584be68f4d379f01cd97a18
Robin Miller <rmillerpdx@gmail.com>
2017-10-02 17:27:30 -0700

    create test for every

--- test/arrayMethods.test.js
+++ test/arrayMethods.test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const forEach = require('../lib/forEach');
+const every = require('../lib/every');
 
 describe('array methods', () => {
     it('forEach', () => {
@@ -9,5 +10,11 @@ describe('array methods', () => {
             called += letter + index;
         });
         assert.equal(called, 'a0b1c2');
+    });
+
+    it('every', () => {
+        const array = [1, 2, 3];
+        let moreThanZero = function(a) {return a > 0;};
+        assert.equal(every(array, moreThanZero), true);
     });
 });
